c63a07600417b8be77fc4a02b2278581229b3113
williamjiamin <lxodgf@gmail.com>
2023-01-06 17:12:13 +0800

    init commit

--- CyberpunkChooserApp/ViewController.swift
+++ CyberpunkChooserApp/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var edgeRunnerImageView1: UIImageView!
+    @IBOutlet weak var edgeRunnerImageView2: UIImageView!
+    @IBOutlet weak var edgeRunnerImageView3: UIImageView!
+    @IBOutlet weak var edgeRunnerImageView4: UIImageView!
+    
+    var edgeRunnerImageView1Num = 0
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+//        edgeRunnerImageView1.image = UIImage(named: "David")
+        edgeRunnerImageView1.image = UIImage(named: "David")
+        edgeRunnerImageView2.image = UIImage(named: "Lucy")
+        edgeRunnerImageView3.image = UIImage(named: "Rebecca")
+        edgeRunnerImageView4.image = UIImage(named: "Kiwi")
+        
     }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
 
+
+        let imageArray = [UIImage(named: "Lucy"),UIImage(named: "Rebecca"),UIImage(named: "Kiwi"),UIImage(named: "David")]
+        
+        
+        edgeRunnerImageView1.image = imageArray[Int.random(in: 0...3)]
+        edgeRunnerImageView2.image = imageArray[Int.random(in: 0...3)]
+        edgeRunnerImageView3.image = imageArray[Int.random(in: 0...3)]
+        edgeRunnerImageView4.image = imageArray[Int.random(in: 0...3)]
+       
+    
+        
+    }
+    
 
 }
 
